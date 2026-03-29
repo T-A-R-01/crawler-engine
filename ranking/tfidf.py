2e0f8ccbd98a8e525700777f88c7e4fdd3626ec1
Tushar Rai <tusharrai0607@gmail.com>
2026-03-29 19:33:44 +0530

    improve scoring

--- ranking/tfidf.py
+++ ranking/tfidf.py
@@ -38,10 +38,17 @@ def compute_score(doc, query, idf):
     doc_words = tokenize(doc)
     query_words = tokenize(query)
 
+    if not doc_words:
+        return 0
+
     score = 0
 
     for q in query_words:
-        tf = doc_words.count(q) / len(doc_words) if doc_words else 0
+        tf = doc_words.count(q) / len(doc_words)
         score += tf * idf.get(q, 0)
+
+    # 🔥 Boost score if query word exists
+    if any(q in doc_words for q in query_words):
+        score *= 5
 
     return score
